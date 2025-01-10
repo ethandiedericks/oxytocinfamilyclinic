1b3a274c8fb44a31b407879602dca9dea25c00da
Ethan Diedericks <diedericksethan@gmail.com>
2025-01-10 20:43:25 +0200

    added lib dir

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const navLinks = [
+    { href: '/', label: 'Home' },
+    { href: '/services', label: 'Services' },
+    { href: '/staff', label: 'Meet our staff' },
+    { href: '/background', label: 'Background' },
+  ];
